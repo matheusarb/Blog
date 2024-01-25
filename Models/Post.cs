f785b6f122f8e507f3dda30186596bde3dde641d
Matheus <mat.araujoribeiro@gmail.com>
2024-01-25 12:29:31 -0300

    post model

---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models;
+
+[Table("[Post]")]
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Summary { get; set; }
+    public string Body { get; set; }
+    public string Slug { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdate { get; set; }
+
+}
