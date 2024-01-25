a20d5b8eb3055a15403359785851d700aaf28a25
Matheus <mat.araujoribeiro@gmail.com>
2024-01-25 20:53:53 -0300

    DeleteTag via menu

--- Screens/TagScreens/DeleteTagScreen.cs
+++ Screens/TagScreens/DeleteTagScreen.cs
@@ -9,6 +9,29 @@ public static class DeleteTagScreen
 {
     public static void Load()
     {
+        Delete();
+    }
 
+    public static void Delete()
+    {
+        var repo = new Repository<Tag>(Database.connection);
+        Console.Clear();
+        System.Console.WriteLine("Escolha o id da tag que deseja deletar: \n");
+        DisplayTags();        
+        short selectedId = short.Parse(Console.ReadLine());
+
+        repo.Delete(selectedId);
+        System.Console.WriteLine("A tag foi deletada. Lista atualizada das tags: \n");
+        DisplayTags();
+    }
+
+    public static void DisplayTags()
+    {
+        var repo = new Repository<Tag>(Database.connection);
+        var tags = repo.GetAll();
+        foreach(var tag in tags)
+        {
+            System.Console.WriteLine($"{tag.Id}| {tag.Name} - {tag.Slug}");
+        }
     }
 }
