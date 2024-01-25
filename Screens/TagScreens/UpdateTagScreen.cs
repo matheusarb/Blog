b6040f5e93a0e646ee7a7832415d4b8cf02f22bc
Matheus <mat.araujoribeiro@gmail.com>
2024-01-25 20:45:24 -0300

    updateTag

--- Screens/TagScreens/UpdateTagScreen.cs
+++ Screens/TagScreens/UpdateTagScreen.cs
@@ -9,6 +9,36 @@ public static class UpdateTagScreen
 {
     public static void Load()
     {
+        Update();
+    }
 
+    public static void Update()
+    {
+        var repo = new Repository<Tag>(Database.connection);
+        
+        System.Console.WriteLine("Escolha o id da tag que você deseja atualizar: ");
+        var tags = repo.GetAll();
+        foreach(var tag in tags)
+        {
+            System.Console.WriteLine($"{tag.Id}| {tag.Name} - {tag.Slug}");
+        }
+        var selectedId = short.Parse(Console.ReadLine());
+
+        Console.Clear();
+
+        System.Console.WriteLine("Digite o novo nome da Tag: ");
+        var newName = Convert.ToString(Console.ReadLine());
+        System.Console.WriteLine("Digite o novo slug da Tag: ");
+        var newSlug = Convert.ToString(Console.ReadLine());
+        Tag updatedTag = new() 
+        {
+            Id = selectedId,
+            Name = newName,
+            Slug = newSlug
+        };
+
+        repo.Update(updatedTag);
+        Tag upTag = repo.GetOne(selectedId);
+        System.Console.WriteLine($"Tag atualizada.\n{upTag.Name} - {upTag.Slug}");
     }
 }
