cbbfa9badbab33e58c42935554046cd2f8b54e6a
Matheus <mat.araujoribeiro@gmail.com>
2024-01-26 12:49:22 -0300

    [add] trycatch

--- Screens/TagScreens/CreateTagScreen.cs
+++ Screens/TagScreens/CreateTagScreen.cs
@@ -25,10 +25,27 @@ public static class CreateTagScreen
         };
 
         var repo = new Repository<Tag>(Database.connection);
-        repo.Create(tag);
+        try{
+            repo.Create(tag);
+        } 
+        catch(Exception ex)
+        {
+            System.Console.WriteLine("Não foi possível criar a Tag");
+            System.Console.WriteLine(ex.Message);
+        }
         Tag insertedTag = repo.GetOne(tag.Id);
         
         System.Console.WriteLine("-----");
-        System.Console.WriteLine($"A tag {insertedTag.Name} foi inserida.");
+        System.Console.WriteLine($"A tag {insertedTag.Name} foi inserida. Lista atualizada: \n");
+        DisplayTags();
+    }
+    public static void DisplayTags()
+    {
+        var repo = new Repository<Tag>(Database.connection);
+        var tags = repo.GetAll();
+        foreach(var tag in tags)
+        {
+            System.Console.WriteLine($"{tag.Id}| {tag.Name} - {tag.Slug}");
+        }
     }
 }
